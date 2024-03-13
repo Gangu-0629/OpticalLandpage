de0588335895488cf1469d95a40777718da77710
Gangu-0629 <atagaragangadhar@gmail.com>
2024-03-13 19:10:53 +0530

    FInal Commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import SunGlasses from './Components/SunGlasses';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const routes=createBrowserRouter([
   {
-    path:"/OpticalLandpage/",
+    path:"/",
     element:<App/>,
     children:[{
       path:"",
